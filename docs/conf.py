9feb1629f563b52894f1e91cc8191c492bbd01cd
Brian Thorne <brian.thorne@nicta.com.au>
2014-11-06 09:35:21 +1100

    Mock gmpy2 and Crypto modules for readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,16 @@
 
 import sys
 import os
+from unittest.mock import MagicMock
+
+
+class Mock(MagicMock):
+
+    @classmethod
+    def __getattr__(cls, name):
+        return Mock()
+
+MOCK_MODULES = ['gmpy2', 'Crypto']
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
