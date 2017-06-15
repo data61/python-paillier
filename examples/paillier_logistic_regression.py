83d2d4576c2bde58e26c597b365e1eccbb58c238
giorgiop <giorgio.patrini@anu.edu.au>
2017-06-15 11:26:03 +1000

    data download&processing

--- examples/paillier_logistic_regression.py
+++ examples/paillier_logistic_regression.py
@@ -31,72 +31,82 @@ import phe as paillier
 
 np.random.seed(42)
 
-# TODO: we should rely on data from an open source data repository
-data_url = 'https://iamtrask.github.io/data/'
-ham_file = 'ham.txt'
-spam_file = 'spam.txt'
-
-# Enron spam dataset #1 urllib
-# url1 = 'http://www.aueb.gr/users/ion/data/enron-spam/preprocessed/enron1.tar.gz'
-# url2 = 'http://www.aueb.gr/users/ion/data/enron-spam/preprocessed/enron2.tar.gz'
+# Enron spam dataset
+url1 = 'http://www.aueb.gr/users/ion/data/enron-spam/preprocessed/enron1.tar.gz'
+url2 = 'http://www.aueb.gr/users/ion/data/enron-spam/preprocessed/enron2.tar.gz'
 
 
-# def download_data(cats):
-#
-#     if (not os.path.isdir('enron1') or not os.path.isdir('enron2')):
-#
-#         print("Downloading data")
-#         # First folder -> train set
-#         foldertar = 'emails.tar.gz'
-#
-#         with urlopen(url1) as remotedata:
-#             with open(foldertar, 'wb') as tar:
-#                 tar.write(remotedata.read())
-#
-#         with tarfile.open(foldertar) as tar:
-#             tar.extractall()
-#         os.remove(foldertar)
-#
-#         # Second folder -> test set
-#         with urlopen(url2) as remotedata:
-#             with open(foldertar, 'wb') as tar:
-#                 tar.write(remotedata.read())
-#
-#         with tarfile.open(foldertar) as tar:
-#             tar.extractall()
-#         os.remove(foldertar)
+def download_data():
+
+    if (not os.path.isdir('examples/enron1') or
+        not os.path.isdir('examples/enron2')):
+
+        print("Downloading 1/2:", url1)
+        # First folder -> train set
+        foldertar = 'examples/emails.tar.gz'
+
+        with urlopen(url1) as remotedata:
+            with open(foldertar, 'wb') as tar:
+                tar.write(remotedata.read())
+
+        with tarfile.open(foldertar) as tar:
+            tar.extractall('examples/')
+        os.remove(foldertar)
+
+        # Second folder -> test set
+        print("Downloading 2/2:", url2)
+        with urlopen(url2) as remotedata:
+            with open(foldertar, 'wb') as tar:
+                tar.write(remotedata.read())
+
+        with tarfile.open(foldertar) as tar:
+            tar.extractall('examples/')
+        os.remove(foldertar)
 
 
-# def preprocess_data():
-#
-#     print("Importing dataset from disk...")
-#     path = 'enron1/ham/'
-#     ham1 = [open(path + f, 'r', errors='replace').read().strip(r"\n").split(" ")
-#             for f in os.listdir(path) if os.path.isfile(path + f)]
-#     path = 'enron1/spam/'
-#     spam1 = [open(path + f, 'r', errors='replace').read().strip(r"\n").split(" ")
-#              for f in os.listdir(path) if os.path.isfile(path + f)]
-#     path = 'enron2/ham/'
-#     ham2 = [open(path + f, 'r', errors='replace').read().strip(r"\n").split(" ")
-#             for f in os.listdir(path) if os.path.isfile(path + f)]
-#     path = 'enron2/spam/'
-#     spam2 = [open(path + f, 'r', errors='replace').read().strip(r"\n").split(" ")
-#              for f in os.listdir(path) if os.path.isfile(path + f)]
-#
-#     # Build training and test sets
-#     X_train = ham1 + spam1
-#     y_train = np.array([-1] * len(ham1) + [1] * len(spam1))
-#     X_test = ham2 + spam2
-#     y_test = np.array([-1] * len(ham2) + [1] * len(spam2))
-#
-#     # Remove small alphanumerical string
-#     pattern = re.compile('[\D_]+')
-#     for i in range(len(X_train)):
-#         X_train[i] = [word for word in X_train[i]
-#                       if len(pattern.sub('', word)) >= 3]
-#     for i in range(len(X_test)):
-#         X_test[i] = [word for word in X_test[i]
-#                      if len(pattern.sub('', word)) >= 3]
+def preprocess_data():
+
+    print("Importing dataset from disk...")
+    path = 'examples/enron1/ham/'
+    ham1 = [open(path + f, 'r', errors='replace').read().strip(r"\n")
+            for f in os.listdir(path) if os.path.isfile(path + f)]
+    path = 'examples/enron1/spam/'
+    spam1 = [open(path + f, 'r', errors='replace').read().strip(r"\n")
+             for f in os.listdir(path) if os.path.isfile(path + f)]
+    path = 'examples/enron2/ham/'
+    ham2 = [open(path + f, 'r', errors='replace').read().strip(r"\n")
+            for f in os.listdir(path) if os.path.isfile(path + f)]
+    path = 'examples/enron2/spam/'
+    spam2 = [open(path + f, 'r', errors='replace').read().strip(r"\n")
+             for f in os.listdir(path) if os.path.isfile(path + f)]
+
+    # Merge and create labels
+    X = ham1 + spam1 + ham2 + spam2
+    y = np.array([-1] * len(ham1) + [1] * len(spam1) +
+                 [-1] * len(ham2) + [1] * len(spam2))
+
+    # Words count
+    count_vect = CountVectorizer(decode_error='replace')
+    X = count_vect.fit_transform(X)
+
+    # Shuffle
+    perm = np.random.permutation(X.shape[0])
+    X, y = X[perm, :], y[perm]
+
+    # Split train and test
+    X_train, X_test = X[-1000:, :50], X[:-1000, :50]
+    y_train, y_test = y[-1000:], y[:-1000]
+
+    return X_train, y_train, X_test, y_test
+
+    # Remove small alphanumerical string
+    # pattern = re.compile('[\D_]+')
+    # for i in range(len(X_train)):
+    #     X_train[i] = [word for word in X_train[i]
+    #                   if len(pattern.sub('', word)) >= 3]
+    # for i in range(len(X_test)):
+    #     X_test[i] = [word for word in X_test[i]
+    #                  if len(pattern.sub('', word)) >= 3]
 
 
 class TimeIt():
@@ -110,46 +120,6 @@ class TimeIt():
 
         time1 = time.time() - self.time0
         print('==> elapsed time: %.2f s' % time1)
-
-
-def get_data():
-
-    # Getting data the remote, if not here yet
-    if not os.path.isfile(ham_file):
-        print("Downloading 1/2", data_url + ham_file)
-        with request.urlopen(data_url + ham_file) as hamdata:
-            with open(ham_file, 'wb') as hamfile:
-                hamfile.write(hamdata.read())
-    if not os.path.isfile(spam_file):
-        print("Downloading 2/2", data_url + spam_file)
-        with request.urlopen(data_url + spam_file) as spamdata:
-            with open(spam_file, 'wb') as spamfile:
-                spamfile.write(spamdata.read())
-
-    # Bring it up from disk
-    print("Importing dataset from disk")
-    with open(ham_file, 'rb') as f:
-        ham = [row[:-2] for row in f.readlines()]
-    with open(spam_file, 'rb') as f:
-        spam = [row[:-2] for row in f.readlines()]
-
-    # Merge and create labels
-    X = ham + spam
-    y = np.array([[1] * len(ham) + [-1] * len(spam)]).reshape(-1)
-
-    # Words count
-    count_vect = CountVectorizer(decode_error='replace')
-    X = count_vect.fit_transform(X)
-
-    # Shuffle
-    perm = np.random.permutation(X.shape[0])
-    X, y = X[perm, :], y[perm]
-
-    # Split train and test
-    X_train, X_test = X[-1000:, :20], X[:-1000, :20]
-    y_train, y_test = y[-1000:], y[:-1000]
-
-    return X_train, y_train, X_test, y_test
 
 
 class PaillierClassifier():
@@ -194,7 +164,8 @@ class PaillierClassifier():
 
 if __name__ == '__main__':
 
-    X, y, X_test, y_test = get_data()
+    download_data()
+    X, y, X_test, y_test = preprocess_data()
 
     timer = TimeIt()
 
@@ -207,7 +178,7 @@ if __name__ == '__main__':
     model = model.fit(X, y)
     timer.tock()
 
-    print("Classify with model in the clear")
+    print("Classify with model in the clear -- what Alice would get having Bob's data locally")
     timer.tick()
     error = np.mean(model.predict(X_test) != y_test)
     timer.tock()
