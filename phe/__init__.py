f9535f8edfed7b43b8e2ff759148bb4da1a10f30
Brian Thorne <brian@thorne.link>
2017-08-19 15:19:04 +1000

    Export package version. Closes #47

--- phe/__init__.py
+++ phe/__init__.py
@@ -1,12 +1,16 @@
+import pkg_resources
+
 from phe.paillier import generate_paillier_keypair
 from phe.paillier import EncodedNumber
 from phe.paillier import EncryptedNumber
 from phe.paillier import PaillierPrivateKey, PaillierPublicKey
 from phe.paillier import PaillierPrivateKeyring
-
 import phe.util
 
 try:
     import phe.command_line
 except ImportError:
     pass
+
+__version__ = pkg_resources.get_distribution('phe').version
+__author__ = 'N1 Analytics'
