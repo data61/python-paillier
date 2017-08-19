362a917455cdfccddabbc69e980c4182ecb9f4f3
Brian Thorne <brian@thorne.link>
2017-08-19 15:42:43 +1000

    Get travis to install before testing.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 def find_version():
     # Note the version is also in the docs/conf.py file
     # We use semantic versioning - semver.org
-    return "1.3.0"
+    return "1.3.1"
 
 
 setup(
