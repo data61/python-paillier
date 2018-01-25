45b9abc7c2b6454371c77c2edd23e90e61493df4
Brian Thorne <brian@thorne.link>
2018-01-25 12:16:38 +1100

    mock wasn't magic enough

--- docs/conf.py
+++ docs/conf.py
@@ -47,7 +47,7 @@ if on_rtd:
     class Mock(MagicMock):
         @classmethod
         def __getattr__(cls, name):
-            return Mock()
+            return MagicMock()
 
     MOCK_MODULES = ['gmpy2', 'numpy']
     sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
